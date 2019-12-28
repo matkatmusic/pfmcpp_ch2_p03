392489897e3f9f6069f1c54c853a73260bd7f873
matkatmusic <matkatmusic@gmail.com>
2019-12-27 22:27:55 -0800

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,65 @@
-/*
- 
- Chapter 2 Part 5 Task:
- User-Defined Types
- 
- click the edit button in the lower right corner.
- 
- 1) write 10 user-defined types, each with a random number of member variables
- try to make the member variables have names that are related to the user-defined type.
- 
- 2) give the member variables relevant data types
- 
- 3) add a couple member functions.
- make the function parameter list for those member functions use some of your User-Defined Types
- 
- 4) make 2 of the 10 user-defined types be nested class
- 
- 5) One of your UDTs should only use UDTs for its member variable types.
- No primitives allowed!
- 
- 6) After you finish defining each type, click the [Compile, Link and Run] button to make sure you don't have any errors.
- 
- share the url into the #ch2-p05 channel once you've got all 10 implemented
- */
+template<typename ...T>
+void ignoreUnused(T&&...) { }
 
 /*
+ Declarations Tasks
+ 
+ 1) click the edit button in the lower right corner.
+ 
+ 2) Write down the names of all of the primitives available in C++ (excluding wchar_t)
+ put them here: 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+3) for each primitive type, write out 3 variable declarations inside the variableDeclaration function.
+    give each declaration an initial value
+        just ignore wchar_t. you do not need to declare 3 variables of type 'wchar_t'
+        'void' is a return type. you do not need to declare 3 variables of type 'void'.
+    at the end of the function, pass each variable to the ignoreUnused function
+ 
+4) write out 10 functions
+    each declaration should have a random number of parameters in the function parameter list.
+    add { ignoreUnused( ); } after each declaration in place of the closing semicolon
+    pass each of your function parameters to the ignoreUnused function. 
+    if your function returns something other than void, add 'return {};' at the end of it.
+    
+5) provide default values for an arbitrary number of parameters in the function parameter list.
+    When naming your parameters, choose names that are relevant to the task implied by the function's name.
+    
+6) in the main function at the end
+    for each of those functions declared, write out how the function would look if called with correct arguments
+ 
+ click the [Compile, link and run...] button.  Clear up any errors or warnings as best you can.
+ share the url into the #week 1 channel.  
+ */
+
+//3)
+void variableDeclarations()
+{
+    //example:
+    int number = 2; //declaration of a primitive named 'number' with an initial value of '2'
+    
+    
+    
+    ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+}
+/*
+ 10 functions
  example:
  */
-struct CarWash            //1) a U.D.T. with a random number of member variables
-{
-    int numSponges = 3;
-    double amountOfSoapUsedPerCar = 2.6; //2) relevant data types
-    unsigned int numCarsProcessed = 0;
-    
-    struct Car                             //4) nested class
-    {
-        bool isAPickupTruck = false;
-        float gasMileage = 26.2f;        //2) relevant data types
-    };
-    
-    void washAndWaxCar( Car car );         //3) member function with a user-defined type as the parameter.  The user-defined type parameter happens to be the nested class.
-    
-    Car myCar;  //5) a member variable whose type is a UDT.
-};
-
+bool rentACar(int rentalDuration, int carType = 0)  //function declaration with random number of arguments, arbitrary number of arguments have default value
+{ 
+    ignoreUnused(rentalDuration, carType); //passing each function parameter to the ignoreUnused() function
+    return {}; //if your function returns something other than void, add 'return {};' at the end of it.
+} 
 
 /*
  1)
@@ -84,8 +101,31 @@ struct CarWash            //1) a U.D.T. with a random number of member variables
  10)
  */
 
-#include <iostream>
 int main()
 {
-    std::cout << "good to go!" << std::endl;
+    //example of calling that function
+    rentACar(6, 2); 
+    
+    //1)
+    
+    //2)
+    
+    //3)
+    
+    //4)
+    
+    //5)
+    
+    //6)
+    
+    //7)
+    
+    //8)
+    
+    //9)
+    
+    //10)
+    
+    
+    return 0;    
 }
